2c4d753ae9115767f609e5737839926f6c18cfc3
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-01-18 17:18:05 -0500

    remove index.css

--- harboris-extension/src/contentScript.jsx
+++ harboris-extension/src/contentScript.jsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 
 /**
  * 1. Create a new <div> and attach it to the document body.
