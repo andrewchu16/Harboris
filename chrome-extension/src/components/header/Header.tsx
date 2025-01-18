6b18176857bc633d26caee8d2d468027ff052d4b
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-01-18 14:22:44 -0500

    Setup routes

--- chrome-extension/src/components/header/Header.tsx
+++ chrome-extension/src/components/header/Header.tsx
@@ -1,16 +1,17 @@
 import styles from "./header.module.css";
+import { Link } from "react-router";
 
 export default function Header() {
     return (
         <header className={styles.header}>
         <h1 className={styles.title}>Harboris</h1>
         <nav className={styles.iconsContainer}>
-          <span className={styles.iconContainer}>
+          <Link className={styles.iconContainer} to="/cart">
             <span className="material-symbols-outlined">shopping_cart</span>
-          </span>
-          <span className={styles.iconContainer}>
+          </Link>
+          <Link className={styles.iconContainer} to="/profile">
             <span className="material-symbols-outlined">account_circle</span>
-          </span>
+          </Link>
         </nav>
       </header>
     )
--- chrome-extension/src/main.tsx
+++ chrome-extension/src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import NavLayout from "./components/layouts/NavLayout/NavLayout";
+import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart";
+import Confirmation from "./pages/confirmation/Confirmation";
+import Product from "./pages/product/Product";
+import ViewOrder from "./pages/viewOrder/ViewOrder";
+import NotFound from "./pages/notFound/NotFound";
+import Profile from "./pages/profile/Profile";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("react-popup")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<NavLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/view-order" element={<ViewOrder />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
